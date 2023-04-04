bbd2a6dd1a6064a07e65dee8a464dbedcaccb9ec
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-04-04 09:36:34 -0600

    Update gettingStarted.spec.js
    getting started tests

--- spec/tests/gettingStarted.spec.js
+++ spec/tests/gettingStarted.spec.js
@@ -1,7 +1,9 @@
 /* eslint-disable wdio/no-pause */
 describe("Getting Started", () => {
-  it("should open a web page", async () => {
+  xit("should open a web page", async () => {
     await browser.url("https://the-internet.herokuapp.com");
     await browser.pause(5000);
   });
+
+  it("should click something", async () => {});
 });
