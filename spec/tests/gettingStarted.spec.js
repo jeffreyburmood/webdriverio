afcb98e2b764db2572dc204e6ac83f46f8c8a058
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-04-06 07:42:20 -0600

    New tests
    and a tweak to the eslint configuration

--- spec/tests/gettingStarted.spec.js
+++ spec/tests/gettingStarted.spec.js
@@ -1,9 +1,33 @@
 /* eslint-disable wdio/no-pause */
-describe("Getting Started", () => {
-  xit("should open a web page", async () => {
-    await browser.url("https://the-internet.herokuapp.com");
+describe('Getting Started', () => {
+  xit('should open a web page', async () => {
+    await browser.url('https://the-internet.herokuapp.com');
     await browser.pause(5000);
   });
 
-  it("should click something", async () => {});
+  xit('should click something', async () => {});
+
+  it('shoukd test checkboxes', async () => {
+    await browser.url('https://the-internet.herokuapp.com');
+    const checkboxlink = $('#content > ul > li:nth-child(6) > a');
+    const checkbox1 = $('#checkboxes > input[type="checkbox"]:nth-child(1)');
+    const checkbox2 = $('#checkboxes > input[type="checkbox"]:nth-child(3)');
+
+    await checkboxlink.click();
+
+    console.log(
+      'Checkbox1 before:' + (await checkbox1.getAttribute('checked'))
+    );
+    console.log(
+      'Checkbox2 before:' + (await checkbox2.getAttribute('checked'))
+    );
+
+    await checkbox1.click();
+    await checkbox2.click();
+
+    console.log('Checkbox1 after:' + (await checkbox1.getAttribute('checked')));
+    console.log('Checkbox2 after:' + (await checkbox2.getAttribute('checked')));
+
+    await browser.pause(3000);
+  });
 });
