1b55aea382d6f688b9bacaf87a6a767765547816
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-04-17 18:42:54 -0600

    Update login.page.js
    new page object

--- pages/demo/login.page.js
+++ pages/demo/login.page.js
@@ -1,3 +1,34 @@
-class LoginPage {}
+class LoginPage {
+  get headingText() {
+    return $('#login-title');
+  }
 
+  get emailLable() {
+    return $('#form-login > div:nth-child(1) > label');
+  }
+
+  get emailField() {
+    return $('#LoginEmail');
+  }
+
+  get passwordLabel() {
+    return $('#form-login > div:nth-child(2) > label');
+  }
+
+  get passwordField() {
+    return $('#loginPassword');
+  }
+
+  get rememberLoginCheckbox() {
+    return $('#rememberLoginChk');
+  }
+
+  get rememberLoginLabel() {
+    return $('#form-login > div.form-check > label');
+  }
+
+  get submitButton() {
+    return $('#form-login > button');
+  }
+}
 module.exports = new LoginPage();
