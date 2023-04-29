d53f6428531442e832f5f2235f5614cd9a11789a
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-04-29 09:44:51 -0600

    new tests for header

--- pages/demo/header.page.js
+++ pages/demo/header.page.js
@@ -27,6 +27,31 @@ class HeaderPage {
   get searchButton() {
     return $('#search-form > button');
   }
+  // modal window selectors
+  get modalWolverineWindow() {
+    return $('##wolverineModal');
+  }
+  get modalWolverineTitleText() {
+    return $('#wolverineModalLabel');
+  }
+  get modalWolverineContentText() {
+    return $('#wolverineModal > div > div > div.modal-body');
+  }
+  get modalWolverineCloseButton() {
+    return $('#wolverineModal > div > div > div.modal-footer > button');
+  }
+  get modalSpidermanWindow() {
+    return $('#spidermanModal');
+  }
+  get modalSpidermanTitleText() {
+    return $('#spidermanModalLabel');
+  }
+  get modalSpidermanContentText() {
+    return $('##spidermanModal > div > div > div.modal-body');
+  }
+  get modalSpidermanCloseButton() {
+    return $('#spidermanModal > div > div > div.modal-footer > button');
+  }
 }
 
 module.exports = new HeaderPage();
--- spec/tests/demo/header.spec.js
+++ spec/tests/demo/header.spec.js
@@ -13,5 +13,23 @@ describe('Header Test Suite', () => {
       'https://glitchitsystem.com/'
     );
   });
-  it('should ', async () => {});
+  it('should open Wolverine modal', async () => {
+    await browser.url('');
+
+    await LoginPage.emailField.setValue('1@2.com');
+    await LoginPage.passwordField.setValue('password');
+    await LoginPage.submitButton.click();
+
+    await HeaderPage.navToggle.click();
+    await HeaderPage.heroFactsLink.click();
+    await HeaderPage.wolverineOption.click();
+
+    //await expect(await HeaderPage.modalWolverineWindow).toBeDisplayed();
+    await expect(await HeaderPage.modalWolverineTitleText).toHaveValue(
+      'Wolverine Fact'
+    );
+    await expect(
+      await HeaderPage.modalWolverineContentText
+    ).toHaveValueContaining('Wolverine made his first comic');
+  });
 });
