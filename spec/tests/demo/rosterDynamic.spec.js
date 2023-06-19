e6b0cb8af2487918d914407cd17746236ed176e0
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-06-19 05:31:47 -0600

    Update rosterDynamic.spec.js
    added new test case for adding multiple items

--- spec/tests/demo/rosterDynamic.spec.js
+++ spec/tests/demo/rosterDynamic.spec.js
@@ -2,7 +2,7 @@ const LoginPage = require('../../../pages/demo/login.page.js');
 const RosterPage = require('../../../pages/demo/roster.page.js');
 
 describe('Dymanic roster suite', () => {
-  it('should create multiple items', async () => {
+  it('should create single item', async () => {
     await browser.url('');
 
     await LoginPage.emailField.setValue('1@2.com');
@@ -26,6 +26,26 @@ describe('Dymanic roster suite', () => {
 
     for (let i = 0; i < heros.length; i++) {
       await expect(await RosterPage.rosterItems[i]).toHaveText(heros[i]);
+    }
+  });
+
+  it('should create multiple items', async () => {
+    const heros = ['Logan', 'Tony', 'Ryan', 'Chris', 'Tom'];
+
+    await browser.url('');
+
+    await LoginPage.emailField.setValue('1@2.com');
+    await LoginPage.passwordField.setValue('password');
+    await LoginPage.submitButton.click();
+
+    for (let i = 0; i < heros.length; i++) {
+      await RosterPage.addHeroField.setValue(heros[i]);
+      await RosterPage.submitButton.click();
+      await browser.pause(2000);
+    }
+
+    for (let i = 0; i < heros.length; i++) {
+      await expect(await RosterPage.rosterItems[i + 5]).toHaveText(heros[i]);
     }
   });
 });
